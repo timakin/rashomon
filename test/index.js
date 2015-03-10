715bd1d2f032a85ef4861615d767303c52dfe894
timakin <timaki.st@gmail.com>
2015-03-10 19:26:55 +0900

    [Add] ParseCode method, tmp commit(Not fixed. Cannot use standard methods of nodejs)

--- test/index.js
+++ test/index.js
@@ -9,7 +9,21 @@ describe('Rashomon', function () {
             assert.equal(Rashomon.testYo(), 'Yo!') ;
         });
     });
+
+    describe('parseCode', function () {
+        it('Test for parsing code', function () {
+            assert.equal(Rashomon.parseCode('./resources/app.js'), parsedApp.parseResult()) ;
+        });
+    });
+
 });
+
+before(function (done) {
+    console.log('やったるぜ╭( ･ㅂ･)و ̑̑');
+    console.log(Rashomon.parseCode('./resources/app.js'));
+    done();
+});
+
 
 after(function (done) {
     console.log('通った。これからも頑張って行こうな╭( ･ㅂ･)و ̑̑');
